91e794a6bc372e9b0fb5a6efe551c2658aa9fb0d
cj <90126284+CeeJank@users.noreply.github.com>
2024-09-07 18:16:20 +0800

    done 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("");
+    let newarr = "";
+    for (let i = arr.length-1; i >= 0; i--){
+        newarr+=arr[i];
+    }
+    return newarr;
 };
 
 // Do not edit below this line
