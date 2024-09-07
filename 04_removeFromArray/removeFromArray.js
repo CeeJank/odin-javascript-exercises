8a4795345ec605c7369606c34a1e61e439cb6cb2
cj <90126284+CeeJank@users.noreply.github.com>
2024-09-07 20:59:41 +0800

    done 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArray = [];
+    arr.forEach((num) => {
+        if (!args.includes(num)){
+            newArray.push(num);
+        }
+    });
+    return newArray;
+
 };
 
 // Do not edit below this line
