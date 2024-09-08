9977b936e36d6750b0af673fe9a19fab345bf347
cj <90126284+CeeJank@users.noreply.github.com>
2024-09-08 14:17:56 +0800

    done 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let title = [];
+    title.push(books[0].title, books[1].title);
+    return title;
 };
 
 // Do not edit below this line
