a81faac6ea021d859c7474f29dbba6b332a02058
cj <90126284+CeeJank@users.noreply.github.com>
2024-09-08 16:23:53 +0800

    done 12, learnt .reduce() .map() and =>

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    return people = people.reduce((oldPerson, youngPerson) => {
+        const oldAge = getAge(oldPerson.yearOfBirth, oldPerson.yearOfDeath);
+        const youngAge = getAge(youngPerson.yearOfBirth, youngPerson.yearOfDeath);
+
+        return oldAge < youngAge ? youngPerson : oldPerson;
+    });  
+
+    
+    
 };
 
 // Do not edit below this line
