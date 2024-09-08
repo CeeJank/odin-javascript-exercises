5b5747c7a76ca866861a3a1a178dd0d853e6dc76
cj <90126284+CeeJank@users.noreply.github.com>
2024-09-08 10:19:15 +0800

    done 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(...number) {
+	let total = 0;
+  for (const num of number){
+    total += num;
+  }
+  return total;
+  //return sum(min, max);
 };
 
-const subtract = function() {
-	
+const subtract = function(min, max) {
+	return min - max;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	array.forEach(num => {
+    total += num;
+  })
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  arr.forEach(num => {
+    total *= num;
+  })
+  return total;
 };
 
-const power = function() {
-	
+const power = function(min, max) {
+	return min ** max;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = num
+  if (num === 0){
+    return 1;
+  }
+	while (num > 1){
+    num--;
+    total *= num;
+  }
+  return total;
 };
 
 // Do not edit below this line
