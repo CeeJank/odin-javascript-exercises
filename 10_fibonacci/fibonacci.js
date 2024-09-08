0d6e9213f159a0db1e5729be1acd633f0709d812
cj <90126284+CeeJank@users.noreply.github.com>
2024-09-08 14:05:28 +0800

    done 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibo) {
+    
+    
+    if (fibo < 0) return "OOPS";
+    if (fibo == 0) return 0;
+    
+    let fib1 = 1;
+    let fib2 = 0;
+    
+    for (let i = 2; i <= fibo; i++){
+        let  fib = fib1 + fib2;
+        fib2 = fib1;
+        fib1 = fib;
+    }
+    return fib1;
 };
 
 // Do not edit below this line
